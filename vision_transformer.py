cb5a1295078badf1737d442134e3c69b3bdd53b2
briankim945 <bsk945@gmail.com>
2025-04-25 11:39:21 -0400

    Create vit_small_imagenet

--- vision_transformer.py
+++ vision_transformer.py
@@ -288,6 +288,13 @@ def vit_small(patch_size=16, **kwargs):
     return model
 
 
+def vit_small_imagenet(patch_size=16, **kwargs):
+    model = VisionTransformer(
+        patch_size=patch_size, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4,
+        qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), num_classes=1000, **kwargs)
+    return model
+
+
 def vit_base(patch_size=16, **kwargs):
     model = VisionTransformer(
         patch_size=patch_size, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4,
